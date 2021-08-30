65de330bebe80c5b5be3a2f421e45b84b792d51a
SoftgearKo <softgear@gmail.com>
2021-08-30 02:48:14 +0000

    Retry later if wrong answer entered. Add edit page

--- Quiz.py
+++ Quiz.py
@@ -24,7 +24,11 @@ def readQuestion():
     questions = {}
     count = 0
     for line in data:
+        if (len(line)<=0):
+            break
         if count%2 == 0:
+            if (len(line)<=1):
+                break
             questions = {}
             questions['Q'] = line
         else:
@@ -63,9 +67,12 @@ def nextQuestion():
     ques = quizSet[questionNo]
     if answer == ques['A']:
         score +=1
+        ok = 'ok'
+    else:
+        ok = 'no'
 
-    answer = ques['A']
-    return render_template('answer.html',score=score,questionNo=questionNo,status="",question=ques['Q'],attempted=attempted,answer= answer)
+    rightanswer = ques['A']
+    return render_template('answer.html',score=score,questionNo=questionNo,status="",question=ques['Q'],attempted=attempted,answer= rightanswer, entered=answer, ok=ok)
 
 @app.route('/answer',methods=['POST','GET'])
 def nextAnswer():
@@ -75,16 +82,43 @@ def nextAnswer():
     questionNo = int(request.form['questionNo'])
     score = int(request.form['score'])
     attempted = int(request.form['attempted'])
+    ok = request.form['ok']
 
     attempted += 1
-    if (attempted > len(quizSet)):
+    randRange.remove(questionNo)
+    if (ok == 'no'):
+        randRange.append(questionNo)
+    elif (len(randRange)<=0):
         percent = int(100 * score / (attempted-1)) 
         return render_template('score.html',score=score, attempted = attempted-1, percent=percent)
-    else: 
-        randRange.remove(questionNo)
-        questionNo = random.choice(randRange)
-        ques = quizSet[questionNo]
-        return render_template('display.html',score=score,questionNo=questionNo,status="",question=ques['Q'],attempted=attempted)
+
+    questionNo = random.choice(randRange)
+    ques = quizSet[questionNo]
+    return render_template('display.html',score=score,questionNo=questionNo,status="",question=ques['Q'],attempted=attempted)
+
+@app.route('/edit',methods=['POST','GET'])
+def edit():
+    global quizSet
+
+    readQuestion()
+    textareadata = ""
+    for question in quizSet:
+        textareadata += question['Q'] + '\n'
+        textareadata += question['A'] + '\n'
+
+    return render_template('edit.html', textareadata=textareadata)
+
+@app.route('/editsubmit',methods=['POST','GET'])
+def editsubmit():
+    textareadata = request.form['textareadata']
+    if(len(textareadata)<3):
+        return edit()
+    
+    txtfile = open('quiz.txt', 'w')
+    n = txtfile.write(textareadata)
+    txtfile.close()
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     readQuestion()
